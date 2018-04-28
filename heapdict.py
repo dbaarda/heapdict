15ef6a10dd85117a984032b961a7a97f1b08a3be
Uriel Hai <urielha@users.noreply.github.com>
2018-04-29 00:07:13 +0300

    Bring heapq functions and adapt them to heapdict internal structure

--- heapdict.py
+++ heapdict.py
@@ -8,7 +8,76 @@ def doc(s):
     def f(g):
         g.__doc__ = s
         return g
+
     return f
+
+
+def _swap(arr, i, j):
+    arr[i], arr[j] = arr[j], arr[i]
+    arr[i][2] = i
+    arr[j][2] = j
+
+
+def _set(arr, i, item):
+    arr[i] = item
+    item[2] = i
+
+
+def heappush(heap, item):
+    """Push item onto heap, maintaining the heap invariant."""
+    item[2] = len(heap)
+    heap.append(item)
+    _siftdown(heap, 0, len(heap) - 1)
+
+
+def heappop(heap):
+    """Pop the smallest item off the heap, maintaining the heap invariant."""
+    lastelt = heap.pop()  # raises appropriate IndexError if heap is empty
+    if heap:
+        returnitem = heap[0]
+        _set(heap, 0, lastelt)
+        _siftup(heap, 0)
+        return returnitem
+    return lastelt
+
+
+# 'heap' is a heap at all indices >= startpos, except possibly for pos.  pos
+# is the index of a leaf with a possibly out-of-order value.  Restore the
+# heap invariant.
+def _siftdown(heap, startpos, pos):
+    newitem = heap[pos]
+    # Follow the path to the root, moving parents down until finding a place
+    # newitem fits.
+    while pos > startpos:
+        parentpos = (pos - 1) >> 1
+        parent = heap[parentpos]
+        if newitem[0] < parent[0]:
+            _set(heap, pos, parent)
+            pos = parentpos
+            continue
+        break
+    _set(heap, pos, newitem)
+
+
+def _siftup(heap, pos):
+    endpos = len(heap)
+    startpos = pos
+    newitem = heap[pos]
+    # Bubble up the smaller child until hitting a leaf.
+    childpos = 2 * pos + 1  # leftmost child position
+    while childpos < endpos:
+        # Set childpos to index of smaller child.
+        rightpos = childpos + 1
+        if rightpos < endpos and not heap[childpos][0] < heap[rightpos][0]:
+            childpos = rightpos
+        # Move the smaller child up.
+        _set(heap, pos, heap[childpos])
+        pos = childpos
+        childpos = 2 * pos + 1
+    # The leaf at pos is empty now.  Put newitem there, and bubble it up
+    # to its final resting place (by sifting its parents down).
+    _set(heap, pos, newitem)
+    _siftdown(heap, startpos, pos)
 
 
 class heapdict(collections.MutableMapping):
@@ -40,7 +109,7 @@ class heapdict(collections.MutableMapping):
         wrapper = [value, key, len(self)]
         self.d[key] = wrapper
         self.heap.append(wrapper)
-        self._decrease_key(len(self.heap)-1)
+        self._decrease_key(len(self.heap) - 1)
 
     def _min_heapify(self, i):
         n = len(self.heap)
